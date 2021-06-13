8d94ecc9441271bfa9948f9176dd0b4f982ae0bf
eddy vazquez <>
2021-06-12 17:40:17 -0700

    Plant finder works. Images removed due to issues with API

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="Search">
-    <h2 class="Search__h2">Ready to meet your matches?</h2>
+    <h2 class="Search__h2">Learn about a random crop!</h2>
       <div class="Search__button" v-on:click="getData()">
-        <span>Yes!</span>
+        <span>Let's go!</span>
         <i class="fas fa-seedling"></i>
       </div>
       <p>Data is recieved through the <a href="https://github.com/Growstuff/growstuff/wiki/API">GrowStuff API</a></p>
@@ -20,9 +20,8 @@
   <!-- So the thing works if and only if you turn off and on the toggle after it loads. GO HERE -->
 <!-- THIS TUTORIAL https://www.youtube.com/watch?v=Qypxpm-llnQ -->
 
-  <div class="Gallery" v-if="list.included && list.data">
+  <div class="Gallery" v-if="list.data">
     <div class="Gallery__case">
-      <img class="Gallery__case__image" v-bind:src="list.included[0].attributes['fullsize-url']">
       <h3 class="Gallery__case__h3">{{this.list.data.attributes.name}}</h3>
       <a class="Gallery__case__p" v-bind:href="this.list.data.attributes['en-wikipedia-url']">Learn more</a>
     </div>
@@ -42,20 +41,24 @@ export default {
   name: 'Results',
   data(){
     return {
-      list: []
+      list: [],
+      apiLink: ""
     }
   },
   components: {
     SearchTiles,
   },
   mounted(){
-    fetch('https://www.growstuff.org/api/v1/crops/330?include=photos')
+    let plantNum = Math.floor(Math.random() * 330);
+    let apiLink = `https://www.growstuff.org/api/v1/crops/${plantNum}`;
+    console.log(apiLink)
+    fetch(apiLink)
     .then(response => {
         return response.json()
     })
     .then((json) => {
       this.list = json
-      console.log(this.list.included[0].attributes['fullsize-url'])
+      console.log(this.list)
       console.log(this.list.data.attributes.name)
       console.log(this.list.data.attributes['en-wikipedia-url'])
       console.log(this.list)
@@ -66,13 +69,16 @@ export default {
   },
   methods: {
     getData(){
-      fetch('https://www.growstuff.org/api/v1/crops/301?include=photos')
+      // fetch('https://www.growstuff.org/api/v1/crops/301?include=photos')
+      let plantNum = Math.floor(Math.random() * 330);
+      let apiLink = `https://www.growstuff.org/api/v1/crops/${plantNum}`;
+      console.log(apiLink)
+      fetch(apiLink)
       .then(response => {
           return response.json()
       })
       .then((json) => {
         this.list = json
-        console.log(this.list.included[0].attributes['fullsize-url'])
         console.log(this.list.data.attributes.name)
         console.log(this.list.data.attributes['en-wikipedia-url'])
       })
@@ -80,6 +86,11 @@ export default {
         console.log("ERROR", err)
       })
     }
+    // randomNumber(){
+    //   let plantNum = Math.floor(Math.random() * 330);
+    //   apiLink= `'https://www.growstuff.org/api/v1/crops/' ${plantNUm} '?include=photos'`;
+    //   console.log(apiLink)
+    // }
   }
 }
 </script>
