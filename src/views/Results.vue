affe06264061d1a4bb6c0f7d1b0709b9e3f4a6eb
eddy vazquez <>
2021-06-13 15:23:16 -0700

    API data is styled

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,35 +1,34 @@
 <template lang="html">
+  <div class="Results" v-if="list.data">
+    <div class="Results__case">
+      <h3 class="Results__case__h3">{{this.list.data.attributes.name}}</h3>
+      <p class="Results__case__p" v-if="list.data.attributes.perennial">This planet is perennial!</p>
+      <p class="Results__case__p" v-else>This plant is not perennial.</p>
+      <a class="Results__case__link" v-bind:href="this.list.data.attributes['en-wikipedia-url']">Learn more on Wikipedia</a>
+      <p class="Results__case__legal">Data is recieved through the <a href="https://github.com/Growstuff/growstuff/wiki/API">GrowStuff API</a></p>
+    </div>
+  </div>
   <div class="Search">
     <h2 class="Search__h2">Learn about another random crop!</h2>
       <div class="Search__button" v-on:click="getData()">
         <span>Show me!</span>
         <i class="fas fa-seedling"></i>
       </div>
-      <p>Data is recieved through the <a href="https://github.com/Growstuff/growstuff/wiki/API">GrowStuff API</a></p>
   </div>
-  <!-- <SearchTiles.vue/> -->
 
-  <!-- <div class="Gallery">
-    <div class="Gallery__case">
-      <img class="Gallery__case__image">
-      <h3 class="Gallery__case__h3">Plant name</h3>
-      <a class="Gallery__case__p">Learn more</a>
-    </div>
-  </div> -->
-
-  <!-- So the thing works if and only if you turn off and on the toggle after it loads. GO HERE -->
-<!-- THIS TUTORIAL https://www.youtube.com/watch?v=Qypxpm-llnQ -->
-
-  <div class="Gallery" v-if="list.data">
+  <!-- <div class="Gallery" v-if="list.data">
     <div class="Gallery__case">
       <h3 class="Gallery__case__h3">{{this.list.data.attributes.name}}</h3>
       <p v-if="list.data.attributes.perennial">This planet is perennial!</p>
       <p v-else>This plant is not perennial.</p>
       <a class="Gallery__case__p" v-bind:href="this.list.data.attributes['en-wikipedia-url']">Learn more</a>
     </div>
-  </div>
+  </div> -->
 
-  <!-- <SearchTiles v-bind:lists="lists"/> -->
+
+
+
+
 
   </template>
 
@@ -60,10 +59,10 @@ export default {
     })
     .then((json) => {
       this.list = json
-      console.log(this.list)
-      console.log(this.list.data.attributes.name)
-      console.log(this.list.data.attributes['en-wikipedia-url'])
-      console.log(this.list.data.attributes.perennial)
+      // console.log(this.list)
+      // console.log(this.list.data.attributes.name)
+      // console.log(this.list.data.attributes['en-wikipedia-url'])
+      // console.log(this.list.data.attributes.perennial)
 
       console.log(this.list)
     })
@@ -76,6 +75,7 @@ export default {
       // fetch('https://www.growstuff.org/api/v1/crops/301?include=photos')
       let plantNum = Math.floor(Math.random() * 330);
       let apiLink = `https://www.growstuff.org/api/v1/crops/${plantNum}`;
+
       console.log(apiLink)
       fetch(apiLink)
       .then(response => {
@@ -83,20 +83,15 @@ export default {
       })
       .then((json) => {
         this.list = json
-        console.log(this.list.data.attributes.name)
-        console.log(this.list.data.attributes['en-wikipedia-url'])
-        console.log(this.list.data.attributes.perennial)
-
+        // console.log(this.list.data.attributes.name)
+        // console.log(this.list.data.attributes['en-wikipedia-url'])
+        // console.log(this.list.data.attributes.perennial)
+        console.log(this.list)
       })
       .catch(err => {
         console.log("ERROR", err)
       })
     }
-    // randomNumber(){
-    //   let plantNum = Math.floor(Math.random() * 330);
-    //   apiLink= `'https://www.growstuff.org/api/v1/crops/' ${plantNUm} '?include=photos'`;
-    //   console.log(apiLink)
-    // }
   }
 }
 </script>
@@ -106,36 +101,41 @@ export default {
 
 @import "../scss/_variables.scss";
 
+  .Results{
+    @media only screen and (min-width: 768px) {display: flex; justify-content: center;}
+    padding: 2rem;
+    &__case{
+      &__h3{
+        font-size: 4rem;
+        font-family: "Aleo", sans-serif;
+        color: $boulder;
+        margin-bottom: 0;
+        margin-top: 2rem;
+      }
+      &__p{
+        font-size: 1rem;
+        font-family: "Aleo", sans-serif;
+        color: $boulder;
+        margin: 0;
+        margin-bottom: 3rem;
+      }
+      &__link{
+        margin-top: 1rem;
+        color: $boulder;
+        font-family: "Aleo", sans-serif;
+        font-size: 1rem;
+      }
+      &__legal{
+        font-size: .9rem;
+        margin-top: .25rem;
+      }
+    }
+  }
+
 
   .Search__button{
     cursor: pointer;
   }
 
-  .Gallery {
-      padding: 1.5rem;
-      border-bottom: 4px solid $pebble;
-      @media only screen and (min-width: 768px) {border-bottom: none;}
-
-
-      &__case {
-          display: flex;
-          flex-direction: column;
-          &__image {
-              width: 100%;
-              margin-top: 1rem;
-          }
-          &__h3 {
-              font-family: 'Aleo', serif;
-              color: $boulder;
-              margin-bottom: 0.75rem;
-              text-decoration: underline;
-              text-decoration-color: $arbory;
-          }
-          &__p {
-              font-family: 'Aleo', serif;
-              margin: 0;
-          }
-      }
-  }
 
 </style>
